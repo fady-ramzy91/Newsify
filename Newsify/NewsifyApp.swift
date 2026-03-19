135ad8bc1f701fdfb95a1bf8b40232218cc6dae1
fady-ramzy91 <fady.h.ramzy@gmail.com>
2026-03-19 17:44:58 +0200

    feat: Add Newsify Project Initialization

---
+++ Newsify/NewsifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsifyApp.swift
+//  Newsify
+//
+//  Created by Fady Ramzy on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NewsifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsifyTests/NewsifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsifyTests.swift
+//  NewsifyTests
+//
+//  Created by Fady Ramzy on 19/03/2026.
+//
+
+import Testing
+@testable import Newsify
+
+struct NewsifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
